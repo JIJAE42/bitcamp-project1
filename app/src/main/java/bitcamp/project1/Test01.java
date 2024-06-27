357ae8af3ab6916e8a59d0dca78f6bd1967d0407
JIJAE42 <wowjd6724@naver.com>
2024-06-27 16:01:53 +0900

    프로젝트 생성

--- app/src/main/java/bitcamp/project1/Test01.java
+++ app/src/main/java/bitcamp/project1/Test01.java
@@ -4,4 +4,8 @@ public class Test01 {
     void test01() {
         System.out.println("git Test 입니다.");
     }
+
+    public static void main(String[] args) {
+        System.out.println("Hello Wold");
+    }
 }
