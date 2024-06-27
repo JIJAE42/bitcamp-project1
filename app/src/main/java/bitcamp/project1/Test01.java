38a1533e859ac5ae679c9d383830c194221f0cd6
JIJAE42 <wowjd6724@naver.com>
2024-06-27 16:26:51 +0900

    .

--- app/src/main/java/bitcamp/project1/Test01.java
+++ app/src/main/java/bitcamp/project1/Test01.java
@@ -8,4 +8,8 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("Hello Wold");
     }
+
+    static void m3() {
+        System.out.println("오호라!!!");
+    }
 }
